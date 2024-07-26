fd658de95fc172553f8c07bdccae446d90452be0
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2024-07-26 18:54:28 +0000

    Fri, Jul 26, 2024, 2:54 PM -04:00

--- circutGame.py
+++ circutGame.py
@@ -1,4 +1,8 @@
 import pygame
 
 while running:
-    for event in pygame.event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+
