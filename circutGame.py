2006b6ec356cb199a3dfff7c1ed820d7b9babd8d
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2024-07-26 18:58:18 +0000

    Fri, Jul 26, 2024, 2:58 PM -04:00

--- circutGame.py
+++ circutGame.py
@@ -1,8 +1,28 @@
+#basic frame of pygame code obtained from https://www.pygame.org/docs/.
 import pygame
 
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
 while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
 
