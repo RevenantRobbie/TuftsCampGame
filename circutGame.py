10215557bf29bfdf4b34f5226755fc896bd90ae0
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2024-07-27 15:04:41 +0000

    Sat, Jul 27, 2024, 11:04 AM -04:00

--- circutGame.py
+++ circutGame.py
@@ -9,6 +9,7 @@ running = True
 
 testing_rectangle = pygame.Rect(screen.get_width()/2,screen.get_height()/2,250,100)
 rectangleClickCount = 0
+pickUpRectangle = True
 
 while running:
     # poll for events
@@ -18,14 +19,17 @@ while running:
             running = False
             #thank you to https://medium.com/@01one/how-to-create-clickable-button-in-pygame-8dd608d17f1b for providing help with the code
 
-        if pygame.mouse.get_pressed()[0]:
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and pygame.mouse.get_pressed():
-                if testing_rectangle.collidepoint(event.pos):
-                    #rectangleClickCount += 1
-                    print("e")
-                    #print(pygame.mouse.get_pos()[0])
-                    #print(type(pygame.mouse.get_pos()))
-                    #testing_rectangle = pygame.Rect(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1], 250,100)
+
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and pygame.mouse.get_pressed():
+            if testing_rectangle.collidepoint(event.pos):
+                rectangleClickCount += 1
+                if rectangleClickCount % 2 == 1:
+                    pickUpRectangle = True
+                else:
+                    pickUpRectangle = False
+                #print(pygame.mouse.get_pos()[0])
+                #print(type(pygame.mouse.get_pos()))
+                #testing_rectangle = pygame.Rect(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1], 250,100)
 
 
 
@@ -34,6 +38,9 @@ while running:
 
     pygame.draw.rect(screen, "white", testing_rectangle)
     # RENDER YOUR GAME HERE
+
+    while pickUpRectangle == True:
+        testing_rectangle = testing_rectangle = pygame.Rect(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1], 250,100)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
