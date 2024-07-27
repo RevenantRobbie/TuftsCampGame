8230bf9cb6dc370a514a8c15d0f21d1b73f6249c
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2024-07-27 14:58:45 +0000

    Sat, Jul 27, 2024, 10:58 AM -04:00

--- circutGame.py
+++ circutGame.py
@@ -19,13 +19,12 @@ while running:
             #thank you to https://medium.com/@01one/how-to-create-clickable-button-in-pygame-8dd608d17f1b for providing help with the code
 
         if pygame.mouse.get_pressed()[0]:
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and pygame.mouse.get_pressed():
-                    if testing_rectangle.collidepoint(event.pos):
-                        #rectangleClickCount += 1
-                        print("e")
-                        #print(pygame.mouse.get_pos()[0])
-                        #print(type(pygame.mouse.get_pos()))
-                        #testing_rectangle = pygame.Rect(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1], 250,100)
+            if testing_rectangle.collidepoint(event.pos):
+                #rectangleClickCount += 1
+                print("e")
+                #print(pygame.mouse.get_pos()[0])
+                #print(type(pygame.mouse.get_pos()))
+                #testing_rectangle = pygame.Rect(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1], 250,100)
 
 
 
