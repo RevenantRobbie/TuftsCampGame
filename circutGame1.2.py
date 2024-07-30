360d84eda2b1098e0f3f70f2dca0b941a4a6cb8a
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2024-07-30 20:48:00 +0000

    Tue, Jul 30, 2024, 4:48 PM -04:00

--- circutGame1.2.py
+++ circutGame1.2.py
@@ -211,6 +211,7 @@ def main():
                             print("clicked on nothing")
                             if len(createdWires) >= 1 and draggedWire != None:
                                 createdWires[-1].startPoint.inUse = False
+                                createdWires[-1].endPoint.inUse = False
                                 createdWires.pop(-1)
 
                                 draggedWire = None
