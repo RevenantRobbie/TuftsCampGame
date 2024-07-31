5aef1055220235ff6dfb0e764aa176fe78a86a96
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2024-07-31 14:41:37 +0000

    Wed, Jul 31, 2024, 10:41 AM -04:00

--- circutGame1.2.py
+++ circutGame1.2.py
@@ -134,7 +134,7 @@ class connectorNode:
     def redrawSelf(self):
         #TODO certain shapes may only have 1 input, scan self.parent to see how many inputs/outputs it has and adjust positions accordingly
         if self.inputOutput == "input1":
-            self.shape = pygame.Rect(self.parent.shape[0]+* 3/4, self.parent.shape[1]+100, 15, 15)
+            self.shape = pygame.Rect(self.parent.shape[0]) +* 3/4, self.parent.shape[1]+100, 15, 15)
         elif self.inputOutput == "input2":
             self.shape = pygame.Rect(self.parent.shape[0]+175, self.parent.shape[1]+100, 15, 15) #input2
         elif self.inputOutput == "output":
