f1f69f1098cad66266d0c6579160c507a20e503e
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2024-07-31 15:20:21 +0000

    Wed, Jul 31, 2024, 11:20 AM -04:00

--- circutGame1.2.py
+++ circutGame1.2.py
@@ -193,8 +193,7 @@ def main():
     #variables declared
     defaultGateShape = pygame.Rect(screen.get_width()/2, screen.get_height()/2,250,100)
     defaultNodeShape = pygame.Rect(0, 0, 15, 15)
-    testingRect = pygame.Rect(screen.get_width(), 100,100,100)
-
+    testingRect = pygame.Rect(screen.get_width()-100, screen.get_height()-200,100,100)
     standardOutputRect = pygame.Rect(screen.get_width()/2, 25, 50, 50)
 
     createdRectangles = []
@@ -319,7 +318,7 @@ def main():
                                     else:
                                         if wire.endPoint.inputOutput[0] == "i":
                                             wire.endPoint.parent.processingInfo[int(wire.endPoint.inputOutput[-1])-1] = False
-                                        elif wire.startPoint.inputOutput[0] == "i":
+                                        elif wire.startPoint.inputOutput[o] == "i":
                                             wire.startPoint.parent.processingInfo[int(wire.startPoint.inputOutput[-1])-1] = False
                                 if Node.parent != shape:
                                     createdNodes.append(Node)
