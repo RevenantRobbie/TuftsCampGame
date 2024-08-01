ac857f81b2ede08b70910d2cdbff80c3bec6d2cd
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2024-08-01 13:58:31 +0000

    Thu, Aug 1, 2024, 9:58 AM -04:00

--- circutGame1.3b.py
+++ circutGame1.3b.py
@@ -383,7 +383,6 @@ def main():
                             len_Node = len(createdNodes) #logic error here, we don't want to iterate through all nodes, only connectedNodes
                             for i,v in enumerate(createdNodes):
                                 if v.parent == shape:
-                                    
 
 
                             for i in range(len_Node):
