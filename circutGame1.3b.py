b68a0b5198ac95452c3d5b84d17b8bcb6a922294
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2024-08-01 05:30:28 +0000

    Thu, Aug 1, 2024, 1:30 AM -04:00

--- circutGame1.3b.py
+++ circutGame1.3b.py
@@ -422,7 +422,7 @@ def main():
                         wire.startPoint.parent.linkedGates[2] = wire.endPoint.parent
                     wire.endPoint.parent.linkedGates[int(wire.endPoint.inputOutput[-1])-1] = wire.startPoint.parent
                     wire.endPoint.parent.indegree += 1
-                    print(wire.startPoint.linkedGates)
+                    print(wire.startPoint.parent.linkedGates)
 
 
 
